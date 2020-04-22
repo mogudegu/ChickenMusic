ab3f87d02f3d44862845e82a100ed8fcee6f47b2
luna <heyu6@ceair.com>
2020-04-22 13:31:44 +0800

    除始代码

---
+++ Tips.md
@@ -0,0 +1,19 @@
+<!-- mogudegu h59 -->
+## 使用技巧
+
+1.去掉点击时间3毫秒的延迟
+    <!-- 安装 -->
+    npm install fastclick -D
+    <!-- 使用 -->
+    import fastclick from fastclick
+    fastclick.attach(document.body)
+
+
+## 使用说明
+1.新建dev分支
+git branch -g dev
+
+
+2.建立本地和仓库的联系
+    a:git init
+    b:git remote add origin https://github.com/mogudegu/ChickenMusic.git
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <template>
-  <div id="app" @touchmove.prevent>
-    <m-header></m-header>
-    <tab></tab>
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
-    <player></player>
+  <div id="app">
+    Hello World
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import MHeader from 'components/m-header/m-header'
-  import Player from 'components/player/player'
-  import Tab from 'components/tab/tab'
 
-  export default {
-    components: {
-      MHeader,
-      Tab,
-      Player
-    }
-  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
